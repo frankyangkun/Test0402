3f96293426d6876e047408d8b94479b71cd016a5
Frank yang <yangkun@sefon.com>
2020-04-08 14:50:10 +0800

    文章相关接口对象层封装。

---
+++ api/api_article.py
@@ -0,0 +1,12 @@
+#导包 requests
+import requests
+
+#新建文章对象类
+class ApiArticle(object):
+    #封装收藏文章方法
+    def api_post_collection(self,url,headers,data):
+        return requests.post(url,headers=headers,json=data)
+
+    #封装取消收藏文章方法
+    def api_del_collection(self,url,headers):
+        return requests.delete(url,headers=headers)
