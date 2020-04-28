f26cdbf2d56549ea4ea743653f35245b7e560009
Frank yang <yangkun@sefon.com>
2020-04-28 22:39:28 +0800

    在Locman的DDT+excel数据驱动模式基础上，结合pytest+Allure实现本地测试报告生成。

--- ddt_excel/DDTReadExcel.py
+++ ddt_excel/DDTReadExcel.py
@@ -13,21 +13,21 @@ class ddtreadexcel(object):
         rows_num = book_sheet.nrows  # 获取行数
         rows0 = book_sheet.row_values(0)  # 获取第一行的数据，作为字典的键，list
         rows0_num = len(rows0)  # 数据个数作为列数
-        print "列数：", rows0_num
+        print ("列数：", rows0_num)
 
         list = []
         for x in range(1, rows_num):
             rows_data = book_sheet.row_values(x)  # 读取每一行的数据
-            print "每行数据：", rows_data  # 每行的数据，[u'chqwh@163.com', u'111111']
+            print ("每行数据：", rows_data)  # 每行的数据，[u'chqwh@163.com', u'111111']
             rows_dic = {}
             for y in range(0, rows0_num):
                 rows_dic[rows0[y]] = rows_data[y]  # 每一行的每列数据加到字典里
-                print "每列：rows_dic[rows0[", y, "]]", rows_dic[rows0[y]]
+                print ("每列：rows_dic[rows0[", y, "]]", rows_dic[rows0[y]])
             list.append(rows_dic)  # 把字典数据加到列表里
-            print "list:", list
+            print ("list:", list)
         return list
 
 
-if __name__ == '__main__':
-    test = ddtreadexcel()
-    test.getdata()
+# if __name__ == '__main__':
+#     test = ddtreadexcel()
+#     test.getdata()
---
+++ ddt_excel/test_demo3.py
@@ -0,0 +1,71 @@
+# -*- coding:utf8 -*-
+"""
+必须在运行模式中用纯python运行，才能生成allure报告
+用unittest和pytest模式运行都不能生成allure报告。
+"""
+import pytest
+import allure
+import os
+import unittest
+from ddt import ddt, data, unpack
+from ddt_excel.DDTReadExcel import ddtreadexcel
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC  # as起别名
+from selenium.webdriver.common.by import By
+
+list1 = ddtreadexcel().getdata()
+
+
+@ddt
+class TestDemo3(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://140.246.137.19:45680/#/login")
+
+    def tearDown(self):
+        self.driver.quit()
+
+    # @pytest.fixture(scope='function')
+    # def login(self):
+    #     print("登录")
+    #     yield
+    #     print("登录完成")
+    #
+    # # @allure.feature('加入购物车')
+    # def test_1(login):
+    #     '''将苹果加入购物车'''
+    #     print("测试用例1")
+    #
+    # # @allure.feature('加入购物车')
+    # def test_2(self):
+    #     '''将橘子加入购物车'''
+    #     print("测试用例2")
+
+    @data(*list1)  # getdata()返回的是list，里面的值是dict的kv值
+    @unpack
+    def test1(self, **dict):  # 解包后得到的是dict类型的kv值
+
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, '/html/body/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[1]/input')))
+        self.driver.find_element_by_xpath(
+            '/html/body/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[1]/input').send_keys(dict.get('username'))
+
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, '/html/body/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[2]/input')))  # 重复操作，后续也需要优化
+        self.driver.find_element_by_xpath(
+            '/html/body/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[2]/input').send_keys(dict.get('password'))
+
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, '/html/body/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[3]/button')))  # 重复操作，后续也需要优化
+        self.driver.find_element_by_xpath(
+            '/html/body/div/div/div/div[1]/div[3]/div/div[2]/div/form/div[3]/button').click()
+
+        self.assertEqual(dict.get('username'), dict.get('password'))
+
+
+if __name__ == "__main__":
+    # 执行pytest单元测试，生成 Allure 报告需要的数据存在 /temp 目录
+    pytest.main(['--alluredir', './temp'])
+    # 执行命令 allure generate ./temp -o ./report --clean ，生成测试报告
+    os.system('allure generate ./temp -o ./report --clean')
