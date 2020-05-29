7e0d0c1546c6fe89149b8fbeca150e2b2e33762a
Frank yang <yangkun@sefon.com>
2020-05-29 16:14:16 +0800

    加入locust自带断言（不是python的assert断言）判断随机请求的正确性。

--- Locust/tests/test_login/testLogin.py
+++ Locust/tests/test_login/testLogin.py
@@ -5,7 +5,7 @@
 而且我的Logger模块里也有，所以看来两者是不能写在一起的
 就算用纯python模式运行，也会因为无法使用pytest的参数化功能而报错，所以locust和pytest彻底无法兼容
 
-需要和python的参数化@parameterized.expand结合使用来达到目的
+需要和python的参数化@parameterized.expand结合使用来达到目的，实现不同用户随机登录
 """
 # 导入相关的locust的类，成员
 from locust import HttpUser, TaskSet, task, between  # HttpLocust已经在1.0版本改为了HttpUser
@@ -20,22 +20,23 @@ import os
 
 # case, param = get_test_data(get_data_path(__file__))  # __file__当前文件路径
 # print("^^^^^^",case,param)  # 可以获取到数据
-def get_data():#处理返回的json中字典内容，达到我们要求的[()]格式
+def get_data():  # 处理返回的json中字典内容，达到我们要求的[()]格式
     datas = ReadJson("test_login/login_more.json").read_json()
     arrs = []
-    print("xxx",datas)
+    print("xxx", datas)
     for i in datas.values():  # 遍历获取所有value,取出'test'的内容
-        print("***",len(i),i)
+        print("***", len(i), i)
 
         for data in i:  # 遍历获取list所有值
             # append本身添加的就是[]，而我们需要的是[()]，所以里面要加上(),否则就把所有内容都添加到一个列表里了
-            print("yyy",data)
+            print("yyy", data)
             arrs.append((data.get("case"),  # data["url"]和get方式的区别是前者如果获取错误会报异常，后者则为空
                          data.get("headers"),
                          data.get("payload"),
                          data.get("expected")
                          ))
     return arrs
+
 
 # 任务类
 class TestLogin(TaskSet):
@@ -58,10 +59,12 @@ class TestLogin(TaskSet):
     #     assert response.json()["resultStatus"]["resultMessage"] == "登录成功！"
     #     # self.logger.error("---------%s error------------" % __name__)
 
-    logger = Logger(file_or_terminal="all", level="DEBUG").getLogger()
+    logger = Logger(file_or_terminal="all", level="ERROR").getLogger()
+    logger.error("---------%s ERROR------------" % __name__)
+
     @parameterized.expand(get_data())
     @task(1)
-    def test_login(self,case,headers,payload,expected):
+    def test_login(self, case, headers, payload, expected):
         url = "http://182.43.224.122:8002/interGateway/v3/user/authentication"
         # headers = {"Content-Type": "application/json"}
         # payload = {
@@ -74,10 +77,12 @@ class TestLogin(TaskSet):
         # response = requests.post(url, headers=headers, json=payload)  # 这个就没有上面的问题
         print("!!!", response.json())
         assert response.json()["resultStatus"]["resultMessage"] == expected
-        self.logger.info("---------%s DEBUG------------" % __name__)
+        self.logger.error("---------%s Locust ERROR Info------------" % __name__)  # 这里的日志打不出来，不知道为什么
+
     # def tearDown(self):
     #     # self.logger.info("---------%s teardown----------" % __name__)
     #     print("***teardown***")
+
 
 class WebSite(HttpUser):
     # task_set = TestDemo  # 声明任务集的类就是上面那个类  task_set在1.0后被弃用
