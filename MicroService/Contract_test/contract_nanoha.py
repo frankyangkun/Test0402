9c5a1e14a38492b22c60b892bea0525941bd9742
Frank yang <yangkun@sefon.com>
2020-06-29 14:43:51 +0800

    pact-python契约测试官方Demo，可做参考。

--- MicroService/Contract_test/contract_nanoha.py
+++ MicroService/Contract_test/contract_nanoha.py
@@ -6,7 +6,7 @@ Nanoha消费者服务和生产者服务之间的契约测试
 模拟了消费者服务可能有『国家』字段，也可能没有该字段的场景，两种场景都通过
 """
 
-import atexit  # 退出时资源自动释放,一般用来做一些资源清理的操作
+import atexit  # 退出时资源自动释放,一般用来做一些资源清理的操作（网上说最好不用它）
 # from atexit import register  # 有时候重启pycharm后会找不到atexit模块，也搜不到，需要重新安装一下register
 import unittest
 from pact import Consumer, Provider
@@ -15,7 +15,7 @@ from MicroService.Contract_test.query import get_cartoon_characters
 # 构造pact对象，定义消费者服务的名字并给它绑定一个生产者服务
 pact = Consumer('Consumer Nanoha').has_pact_with(Provider('Provider'))  # 为消费者绑定一个生产者，名字可随便取
 pact.start_service()  # 启动pact服务(Start the external Mock Service.)
-atexit.register(pact.stop_service)  # 注册退出时，关闭pact服务,stop_service不能要括号
+atexit.register(pact.stop_service)  # 注册退出时，关闭pact服务,stop_service不能要括号（register的固定写法）
 
 
 class GetNanohaInfoContract(unittest.TestCase):
