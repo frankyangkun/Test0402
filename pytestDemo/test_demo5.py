82ce8f8ebb8b77f864fdb335c71bb2c9c872d85c
Frank yang <yangkun@sefon.com>
2020-07-15 16:32:38 +0800

    解决契约测试脚本生成Allure报告报错的问题 总结一下用法，如果直接在pycharm里使用，下面两条命令可以直接写在main中，如果要在ci中使用，必须使用命令行执行 1、先用pytest生成测试数据，pytest --alluredir=temp test_demo4.py 2、有时候找不到allure命令，source ~/.bash_profile 生效一下 3、生成allure报告，allure generate ./temp -o ./report --clean 4、注意：jenkins中添加以上命令 5、注意：不能把命令写在main中，然后用python3 xx.py的方式执行脚本

--- pytestDemo/test_demo5.py
+++ pytestDemo/test_demo5.py
@@ -7,7 +7,7 @@ scope="package"  包内的所有类都可以使用
 scope="session"  所有包的所有类都可以使用
 """
 import pytest
-
+import os
 
 @pytest.fixture()  # 将该函数作为参数传给其他函数，默认是scope="function"
 def login():
@@ -46,3 +46,16 @@ class TestDemo5(object):
 
 if __name__ == '__main__':
     pytest.main(["-m","others"])
+    # # 执行pytest单元测试，生成Allure报告，需要的数据存在/temp目录
+    # pytest.main(['--alluredir', './temp'])
+    # # 执行命令allure generate ./temp -o ./report --clean，生成测试报告
+    # os.system('allure generate ./temp -o ./report --clean')
+    # # 以上两条命令在pycharm pytest模式下运行只能生成temp里的报告数据，无法生成报告
+    # # 必须用pycharm python模式运行才能生成报告
+    # =================================================================================================
+    # 总结一下用法，如果直接在pycharm里使用，上面两条命令可以直接写在main中，如果要在ci中使用，必须使用命令行执行
+    # 1、先用pytest生成测试数据，pytest --alluredir=temp test_demo4.py
+    # 2、有时候找不到allure命令，source ~/.bash_profile 生效一下
+    # 3、生成allure报告，allure generate ./temp -o ./report --clean
+    # 4、注意：jenkins中添加以上命令
+    # 5、注意：不能把命令写在main中，然后用python3 xx.py的方式执行脚本
